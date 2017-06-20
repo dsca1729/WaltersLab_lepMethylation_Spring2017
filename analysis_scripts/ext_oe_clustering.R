6376941a969592e4145dcb90657c32a8ecb7b4d9
dsca1729 <dcuellar@ku.edu>
2017-06-20 17:37:17 -0500

    Finalize Data Processing
    Finalized Histograms
    Tabular data from mclust object pulled
    Repository organized and consolidated

--- analysis_scripts/ext_oe_clustering.R
+++ analysis_scripts/ext_oe_clustering.R
@@ -1,25 +1,39 @@
-#Script assumes working directory (wd) is contains the entirety of data-sets of interest
+
+#setwd("C:/Documents/GitHub/WaltersLab_lepMethylation_Spring2017/input_files/CpGOE_data")
+#setwd("C:/Documents/GitHub/WaltersLab_lepMethylation_Spring2017/input_files/transcriptome_data")
+#setwd("C:/Documents/GitHub/WaltersLab_lepMethylation_Spring2017/input_files/kawahara_data")
+#setwd("C:/Documents/GitHub/WaltersLab_lepMethylation_Spring2017/input_files/bazinet_data")
+
 fileNames <- list.files()
 
-#orgNames <- c("A_Transitella", "B_Anynana1", "B_Anynana2", "Bombyx_Mori1", "Bombyx_Mori ASM", "C._Cecrops", "C._Supressalis", "D_Plexippus1", "D. Plexippus3", "H. Erato", "H. Melpomene1", "H. Melpomene2", "L. Accius", "M. Sexta", "M. Cinxia", "O. Brumata", "P. Glaucus", "P. Machaon", "P. Polytes", "P. Xuthus Pap", "P. Xuthus Pxut", "P. Sennae", "P. Interpunctella", "P. Xylostella") 
+kawahara_Names <- c("Pterodecta felderi", "Notoplusia minuta", "Therinia lactucina", "Morpheis mathani", "Megalopyge tharops", "Aepytus sp.", "Dalcera abrasa", "Lacosoma ludolpha", "Adhemarius daphne", "Nothus lunus", "Artace sp.", "Semomesia campanea", "Zeuzerodes maculata", "Eacles sp.", "Macrosoma sp.", "Hylephila phyleus", "Hemiargus ceraunus", "Lantanophaga pusillidactyla", "Macaria distribuaria", "Nemoria lixaria", "Urodus parvula", "Megathymus yuccae", "Pseudothyris sepulchralis", "Phyllocnistis citrella", "Cnaphalocrocis medinalis", "Grapholita dimorpha", "Papilio glaucus", "Papilio polytes", "Thubana sp.", "Anigraea sp.", "Lyssa zampa", "Dichomeris sp.", "Nemophora sp.", "Manoba major", "Eudocima salaminia", "Xylophanes tersa")
+refGenome_Names <- c("Amyelois transitella", "Bicyclus anynana nBA", "Bicyclus anynana v1.2", "Bombyx mori", "Bombyx mori ASM", "Calycopis cecrops", "Chilo suppressalis", "Danaus plexippus", "Danaus plexippus v3", "Heliconius erato", "Heliconius melpomene", "Heliconius melpomene 2", "Lerema accius", "Manduca sexta", "Melitaea cinxia", "Operophtera brumata", "Papilio glaucus", "Papilio machaon", "Papilio polytes", "Papilio xuthus 1", "Papilio xuthus 2", "Phoebis sennae", "Plodia interpunctella", "Plutella xylostella")
+bazinet_Names <- c("Dryadaula visaliella", "Palaephatus luteolus", "Phymatopus californicus", "Thyridopteryx ephemeraeformis", "Tineola bisselliella", "Tischeria quercitella", "Acanthopteroctetes unifascia", "Eudarcia simulatricella", "Ptyssoptera sp. AB-2015", "Acanthopteroctetes unifascia", "Acanthopteroctetes unifascia", "Agathiphaga queenslandensis", "Agathiphaga queenslandensis", "Agathiphaga queenslandensis", "Andesiana lamellata", "Azaleodes micronipha", "Dyseriocrania griseocapitella", "Dryaduala visaliella", "Enteucha acetosae", "Eudarcia simulatricella", "Heterobathmia pseuderiocrania", "Lophocorona astiptica", "Metaphatus ochraceus","Metaphatus ochraceus","Metaphatus ochraceus", "Palaephatus luteolus", "Phymatopus californicus", "Palaephatus nielseni", "Thyridopterix ephemeraeformis", "Tineola bisselliella", "Neopseustis meyricki", "Pseudopostega quadristigella", "Tischeria quercitella", "Tegeticula yuccasella", "Ptyssoptera sp. AB-2015")
+trans_Names <- c("Chilo suppressalis", "Heliconius melpomene", "Melitaea cinxia", "Manduca sexta", "Plutella Xylostella")
 
 library(mclust)
 
 #generate and plot individual density clusters
 fit_and_plot <- function(x)
 {
-  #z <- densityMclust(x$CpGOE, G=1:2)
-  plot.dens(clus = z)
+  z <- densityMclust(x$CpGOE, G=1:2)
+  plot.dens(z)
 }
 
 filter_geneLength <- function(x)
 {
-  x <- subset(x, x$gene_length >= 200)
+  x <- subset(x, x$gene_length >= 100)
 }
 
+filter_OE <- function(x)
+{
+  x <- subset(x, x$CpGOE <= 3.5)  
+}
 
-
-
+tab_mean <- function(x)
+{
+  p <- clustData2[[x]]$parameters$mean
+}
 
 #Density Plotting Function
 plot.dens <- function(clus, ...) {
@@ -39,15 +53,19 @@ plot.dens <- function(clus, ...) {
 }
 
 #read in all files for usage.
-dataList <- lapply(fileNames, read.delim)
-names(dataList) <- fileNames
-dataList <- lapply(dataList, na.omit)
-dataList <- lapply(dataList, filter_geneLength)
+dataList2 <- lapply(fileNames, read.delim)
+names(dataList2) <- fileNames
+dataList2 <- lapply(dataList2, na.omit)
+dataList2 <- lapply(dataList2, filter_geneLength)
+dataList2 <- lapply(dataList2, filter_OE)
 
 
 #generating density clusters
-#pdf("histograms_kawahara.pdf")
-clustData <- lapply(dataList, fit_and_plot)
-#dev.off()
+pdf("histograms_transcriptomes.pdf")
+lapply(dataList2, fit_and_plot)
+dev.off()
 
-
+#generating mean tables
+trans_mean <- lapply(1:5, tab_mean)
+kawahara_mean <- lapply(1:, tab_mean)
+bazinet_mean <- lapply(1:35, tab_mean)
